2e823511cc60471cc9cd581f9c775e39a0aa1c57
李轩0122 <2249908118@qq.com>
2025-05-01 01:16:20 +0800

    用户短信登录代码开发

---
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -0,0 +1,57 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取请求头中的token
+        //HttpSession session = request.getSession();
+        String token = request.getHeader("authorization");
+        log.info("得到请求头中的token：{}",token);
+        if (StrUtil.isBlank(token)) {
+            return true;
+        }
+        //2.获取redis中的用户
+        //Object user = session.getAttribute("user");
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
+        //3.判断用户是否存在
+        if(userMap.isEmpty()){
+            return true;
+        }
+        //将查询到的hash数据 转为userdto对象
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        //5.存在，保存用户信息到Threadlocal
+        UserHolder.saveUser(userDTO);
+        //6.刷新token有效期
+        //log.info("请求头中的token：{}",token);
+        stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        //7.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
