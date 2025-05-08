0a67cb6950c9ebb26bc749926236799233bc499a
李轩0122 <2249908118@qq.com>
2025-05-08 15:15:40 +0800

    GEO实现附件商铺代码开发

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,15 +1,26 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.RedisData;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -21,6 +32,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void testIdWorker() throws InterruptedException {
@@ -51,4 +65,46 @@ class HmDianPingApplicationTests {
 
     }
 
+    @Test
+    void loadShopData(){
+
+        // 1. 查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2. 把店铺分组，按照typeId分组， typeId一致的放在一个集合中
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3. 分批完成写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            //获取类型id
+            Long typeId = entry.getKey();
+            //获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+
+            String key = SHOP_GEO_KEY + typeId;
+            //写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+               // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()),shop.getId().toString())
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
